1b71f1afc8a39ff633c1208e492d1410af31c0ae
Tony Elkharrat <tonyel432@gmail.com>
2019-06-26 00:33:43 +0300

    Upload of the project

---
+++ SimonSay/app/src/main/java/com/example/simonsay/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.simonsay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
